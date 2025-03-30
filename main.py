aacbe1e17561d49af139c786a5d81d357ccf58e9
BSchotanes <bryan@schotanes.com>
2025-03-30 12:01:27 -0700

    Group Opject updating and Drawing, Added movement

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     print("Starting Asteroids!")
@@ -18,9 +23,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
+        updatable.update(dt)
+
         screen.fill((0,0,0))
-        player.draw(screen)
+
+        for object in drawable:
+            object.draw(screen)
+            
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
